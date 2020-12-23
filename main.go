5d8963afd502b9f0251eb9c3cd5fe052233e62a2
Simone <startae14@gmail.com>
2020-12-24 00:52:24 +0100

    3. use the Print methods to print into console

--- main.go
+++ main.go
@@ -2,6 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello Front End Masters!")
-}
+// func main() {
+	// fmt.Println("Hello Front End Masters!")
+// }
--- practice.go
+++ practice.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	fmt.Print("Hello again")
+	fmt.Print("Hello again")
+	fmt.Printf("Hello, my name is %s. It is December %d", "Simone", 24)
 }
